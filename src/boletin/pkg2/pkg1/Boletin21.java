757497af50f4c7d078b967568e73ff8ed654a281
Alejandro gonzalez <>
2017-10-09 09:03:05 +0200

    commit ejercicio 2.2

---
+++ src/boletin/pkg2/pkg1/Boletin21.java
@@ -0,0 +1,14 @@
+
+package boletin.pkg2.pkg1;
+
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        float lado = 3f ;
+               
+        System.out.println("area = "+ lado*lado);
+        
+        
+    }
+    
+}
