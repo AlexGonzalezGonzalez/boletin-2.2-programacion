32ff3c675c73ed8bf62497c9a5ff4d6659d76a55
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2017-10-19 09:41:25 +0200

    añadiendo brillibrilli

--- src/boletin/pkg2/pkg1/Boletin21.java
+++ src/boletin/pkg2/pkg1/Boletin21.java
@@ -4,9 +4,9 @@ package boletin.pkg2.pkg1;
 public class Boletin21 {
 
     public static void main(String[] args) {
-        float lado = 3f ;
+        float lado = 45f ;
                
-        System.out.println("area = "+ lado*lado);
+        System.out.println("La gran area es de "+ lado*lado+"MUAJAJAJAJAJAAaaAAAaaAA");
         
         
     }
